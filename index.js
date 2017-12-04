82ef21487cd382df09b554cc77b6be557962525b
iddqd388 <34138521+iddqd388@users.noreply.github.com>
2017-12-04 20:29:49 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello Fucking abracadabra World!");
+    response.end("Hello Fucking abracadabra World is mine!");
 
 });
 
